fd984fee973da277942746212db1d0b42f9c2b4e
aashish khanal <aashishkhanal503@gmail.com>
2022-09-15 11:09:03 +0545

    added sql

---
+++ store.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO user(`firstname`,`lastname`,`email`,`password`,`address`) 
+VALUES
+("Anjan","Shrestha","anjanshrestha2002@gmal.com","anjan","Pokhara"),
+("Anja1","Shrestha","anjanshrestha2002@gmal.com","anjan","Pokhara"),
+("Anjan2","Shrestha","anjanshrestha2002@gmal.com","anjan","Pokhara"),
+("Anjan3","Shrestha","anjanshrestha2002@gmal.com","anjan","Pokhara"),
+("Anjan4","Shrestha","anjanshrestha2002@gmal.com","anjan","Pokhara"),
+("Anjan5","Shrestha","anjanshrestha2002@gmal.com","anjan","Pokhara");
+
+SELECT * FROM user;
